b9e2a32faccfc427be6f17971808626a08b3dbb7
Hassan Mohamed <hmohammed.c@tetco.sa>
2023-09-06 12:05:11 +0300

    Add identity server

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,15 @@
+﻿// Ignore Spelling: Api
+
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<Client> Clients => new Client[] { };
+    public static IEnumerable<ApiScope> ApiScopes => new ApiScope[] { };
+    public static IEnumerable<ApiResource> ApiResources => new ApiResource[] { };
+    public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] { };
+    public static List<TestUser> TestUsers => new List<TestUser> { };
+}
