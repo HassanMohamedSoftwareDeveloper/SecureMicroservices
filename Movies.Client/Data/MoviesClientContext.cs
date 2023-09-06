0264b52030a48543ef7124ef16e80f6dd94a93b3
Hassan Mohamed <hmohammed.c@tetco.sa>
2023-09-06 15:22:51 +0300

    Add movie client application

---
+++ Movies.Client/Data/MoviesClientContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movies.Client.Data;
+
+public class MoviesClientContext : DbContext
+{
+    public MoviesClientContext(DbContextOptions<MoviesClientContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Models.Movie> Movie { get; set; } = default!;
+}
