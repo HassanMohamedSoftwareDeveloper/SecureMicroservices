8605ae892daf0147d7011aee83db05368ceec871
Hassan Mohamed <hmohammed.c@tetco.sa>
2023-09-06 17:26:49 +0300

    update config.cs for IS4 OpenID Connect with MVC Interactive Client and TestUser

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,7 +1,10 @@
 ﻿// Ignore Spelling: Api
 
+using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
+using System.Security.Claims;
 
 namespace IdentityServer;
 
@@ -18,13 +21,54 @@ public static class Config
                 new Secret("secret".Sha256())
             },
             AllowedScopes={ "movieAPI" }
-        }
+        },
+
+         new Client()
+         {
+            ClientId="movie_mvc_client",
+            ClientName="Movies MVC Web App",
+            AllowedGrantTypes=GrantTypes.Code,
+            AllowRememberConsent=false,
+            RedirectUris=new List<string>()
+            {
+                "https://localhost:7244/signin-oidc"
+            },
+            PostLogoutRedirectUris=new List<string>()
+            {
+                "https://localhost:7244/signout-callback-oidc"
+            },
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+            AllowedScopes=new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+            }
+        },
     };
     public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
     {
         new ApiScope("movieAPI","Movie API")
     };
     public static IEnumerable<ApiResource> ApiResources => new ApiResource[] { };
-    public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] { };
-    public static List<TestUser> TestUsers => new List<TestUser> { };
+    public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+    };
+    public static List<TestUser> TestUsers => new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId=Guid.NewGuid().ToString(),
+            Username="admin",
+            Password="admin",
+            Claims=new List<Claim>
+            {
+                new Claim(JwtClaimTypes.GivenName,"admin"),
+                new Claim(JwtClaimTypes.FamilyName,"mohamed")
+            }
+        },
+    };
 }
